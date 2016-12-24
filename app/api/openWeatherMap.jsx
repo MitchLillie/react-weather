e2536069bfd171b23571011ef89f0ae6af5b1892
Mitch Lillie <mitch@mitchlillie.com>
2016-12-24 08:16:11 -0500

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var axios = require('axios')
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=46cf170f638fac2fa8d94bed40252cf6&units=imperial'
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location)
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`
+
+    return axios.get(requestUrl).then(function success (res) {
+      debugger
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message)
+      } else {
+        return res.data.main.temp
+      }
+    }, function error (res) {
+      throw new Error(res.data.message)
+    })
+  }
+}
---
+++ app/components/Weather.jsx
@@ -0,0 +1,52 @@
+var React = require('react')
+var WeatherForm = require('WeatherForm')
+var WeatherMessage = require('WeatherMessage')
+var openWeatherMap = require('openWeatherMap')
+
+var Weather = React.createClass({
+  getInitialState: function () {
+    return {
+      isLoading: false
+    }
+  },
+  handleSearch: function (location) {
+    debugger
+    this.setState({
+      isLoading: true
+    })
+    var self = this
+    openWeatherMap.getTemp(location).then(function success (temp) {
+      self.setState({
+        location: location,
+        temp: temp,
+        isLoading: false
+      })
+    }, function error (errorMsg) {
+      self.setState({isLoading: false})
+      alert(errorMsg)
+    })
+  },
+  render: function () {
+    var {isLoading, location, temp} = this.state
+
+    function renderMessage () {
+      if (isLoading) {
+        return <h3>Fetching weather...</h3>
+      } else if (temp && location) {
+        return <WeatherMessage location={location} temp={temp} />
+      }
+    }
+
+    return (
+      <div>
+        <h3>
+          Weather component
+        </h3>
+        <WeatherForm onSearch={this.handleSearch} />
+        {renderMessage()}
+      </div>
+    )
+  }
+})
+
+module.exports = Weather
